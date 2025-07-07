ded1b765192b077d4f10fbe90a90266c1e34f089
hoquocviet <hoquocviet263@gmail.com>
2025-07-07 22:06:53 +0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -95,9 +95,7 @@ const routes: Routes = [
     HttpClientModule,
     ReactiveFormsModule,
   ],
-  providers: [
-    /*{ provide: LocationStrategy, useClass: HashLocationStrategy }*/
-  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
